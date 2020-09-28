aed98d3aca6e2bf4324f8f46cd1915d0d59a35f4
misael-macAir <hola@landercorp.mx>
2020-09-28 15:18:03 -0500

    Initial Commit

---
+++ Shared/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  Shared
+//
+//  Created by Francisco Misael Landero Ychante on 28/09/20.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Francisco Misael Landero Ychante on 28/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
